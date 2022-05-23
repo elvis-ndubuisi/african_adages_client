26cab78b6aaa78a033e42c03e582fae160c79858
SimplyElvis <provictor.ie@gmail.com>
2022-05-23 14:33:45 +0100

    Footer components.

---
+++ src/components/Footer.svelte
@@ -0,0 +1,11 @@
+<footer>
+    <section class="comp-wrapper">footer</section>
+</footer>
+
+<style>
+    footer {
+        background-color: hsl(var(--clr-primary));
+        color: hsl(var(--clr-white));
+        padding: 1rem 0;
+    }
+</style>
